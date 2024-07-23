524cb261142cfd7b00b99075cb20fcde374059da
Vasco <vascoo.bras@gmail.com>
2024-07-23 17:48:25 +0100

    Fixed Gradle configuration for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,11 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,5 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Smart Car Dashboard"
+rootProject.name = "SmartCarDashboard"
 include(":app")
